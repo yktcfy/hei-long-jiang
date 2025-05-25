7bb47a279a591b0f8fa1754c5cce1debad983ccc
yktcfy <zjyktcfy@qq.com>
2025-05-25 17:21:49 +0800

    Update CaijiHeilongjiangip.sh

--- CaijiHeilongjiangip.sh
+++ CaijiHeilongjiangip.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "开始测试······"
   echo "在5秒内输入1~4可选择城市"
   echo "请选择城市："
-  echo "1. 北京（Beijing_liantong）"
+  echo "1. 黑龙江（Heilongjiang_liantong）"
   read -t 5 -p "超时未输入,将按默认设置测试" city_choice
 
   if [ -z "$city_choice" ]; then
@@ -17,8 +17,8 @@ fi
 # 设置城市和相应的stream
 case $city_choice in
     1)
-        city="Beijing_liantong"
-        stream="rtp/239.3.1.250:8001"
+        city="Heilongjiang_liantong"
+        stream="rtp/229.58.190.14:5000"
         ;;
     0)
         # 逐个处理{ }内每个选项
